bc2af024610e4d7d72ea908cb708bfae42879d58
ABARNA S <abarnasanjay162@gmail.com>
2026-08-31 11:31:15 +0530

    Revise Student table and adjust SQL operations
    Updated Student table structure and modified queries.

--- answer.sql
+++ answer.sql
@@ -1,23 +1,26 @@
+CREATE DATABASE IF NOT EXISTS CollegeDB;
+USE CollegeDB;
+
+DROP TABLE IF EXISTS Student;
 CREATE TABLE Student (
     StudentID INT PRIMARY KEY,
-    StudentName VARCHAR(50),
+    StudentName VARCHAR(20) NOT NULL,
     Gender VARCHAR(10),
     DepartmentID INT
 );
+desc Student;
 
 INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
 VALUES
 (1001, 'Arun', 'Male', 101),
 (1002, 'Divya', 'Female', 102),
 (1003, 'Karthik', 'Male', 101);
-
-SELECT * FROM Student;
+select * from Student;
 
 UPDATE Student
 SET DepartmentID = 103
-WHERE StudentName = 'Karthik';
-
-DELETE FROM Student
+WHERE StudentID = 1003;
+  
+DELETE from Student
 WHERE StudentID = 1002;
-
-SELECT * FROM Student;
+select * from Student;
